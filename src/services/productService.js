b1f9e5a0602bade097c38da30a71e75e38ee5cbc
Manitej Chowdary Gadagottu <2300031033@kluniversity.in>
2025-09-25 12:19:55 +0530

    Update API base URL for product service

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://35.154.47.218:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://3.80.106.115:8080/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
